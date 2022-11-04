8f2057b34a50b60856344ddea530677ad76f4828
Stehlampe2020 <94976382+Stehlampe2020@users.noreply.github.com>
2022-11-04 16:51:05 +0100

    Update seedcracker0.py

--- seedcracker0.py
+++ seedcracker0.py
@@ -8,11 +8,11 @@ def test(seed, nums=(1, 2, 3)): # Some example numbers as default in nums
     if [random.randint(0, 99) for i in range(len(nums))] == list(nums): # Use list comprehension to generate the numbers and check them against the expected numbers.
         return seed # Return the seed if they match and
     return None # return nothing (None) if they don't match.
-
-numbers = (13, 37, 42) # The tuple here is the numbers that the code checks for in all seeds.
+numbers = (13, 37, 42, 80, 81, 55, 71, 21, 45, 42) # This line provides ten values and only one seed is found.
+numbers = (13, 37, 42) # The tuple here is the numbers that the code checks for in all seeds. This line provides only three values and several seeds are found.
 print(f'Searching for seeds that generate {numbers}...')
 for i in range(0xffffff):
+    print(f'Testing seed: {i}', end="\r")
     t = test(i, numbers)
     if t:
-        print(f'Possible seed: {t}')
-
+        print(f'Possible seed: {t}   ')
